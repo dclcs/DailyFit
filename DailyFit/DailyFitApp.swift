1cad594c0c2704ffeef091a4584f4c7d7c9d93d6
dclcs <dcl.outlook.1016@gmail.com>
2023-07-15 20:49:54 +0800

    初始化项目

---
+++ DailyFit/DailyFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyFitApp.swift
+//  DailyFit
+//
+//  Created by cl d on 2023/7/15.
+//
+
+import SwiftUI
+
+@main
+struct DailyFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
